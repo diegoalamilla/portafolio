bc6054547544724781bfe977256241f62476f179
diegoalamilla <diego.alamilla123@gmail.com>
2025-02-24 00:46:32 +0000

    Deploy script fixed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
 sudo systemctl stop nginx
 
-sudo systemctl stop ngrok
+sudo ngrok service stop
 
 git pull origin main
 
 sudo cp -r dist/* /var/www/html
 
 sudo systemctl start nginx
+
+sudo ngrok service start
+
+export NGROK_AUTHTOKEN=$1
 
 ngrok http http://localhost:80
